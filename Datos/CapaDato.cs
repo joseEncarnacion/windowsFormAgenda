ca52b99461ec89dc7aa2f3fd0c63e94184c67a66
Jose Luis Encarnacion <jose.lencarnacion1@gmail.com>
2023-10-27 23:26:34 -0400

    initial file

---
+++ Datos/CapaDato.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Configuration;
+using System.Runtime.Remoting.Messaging;
+using System.Windows.Forms;
+
+namespace Datos
+{
+    public class CapaDato
+    {
+        // conexion de la base de datos
+
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+
+      
+
+        public void probarConenexion()
+        {
+            try
+            {
+                conn.Open();
+                MessageBox.Show("conectados");
+            }
+            catch (Exception ex)
+            {
+                throw(ex);
+                
+            }finally { conn.Close(); }
+        }
+
+
+
+        public DataTable ListarConexion()
+        {
+            SqlCommand cmd = new SqlCommand("sp_Listar", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public void CrearContacto()
+        {
+            
+        }
+
+        public void Eliminar()
+        {
+
+        }
+
+        public void Actualizar()
+        {
+
+        }
+    }
+}
---
+++ Presentacion/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Presentacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrueba = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPrueba
+            // 
+            this.btnPrueba.Location = new System.Drawing.Point(152, 93);
+            this.btnPrueba.Name = "btnPrueba";
+            this.btnPrueba.Size = new System.Drawing.Size(75, 23);
+            this.btnPrueba.TabIndex = 0;
+            this.btnPrueba.Text = "button1";
+            this.btnPrueba.UseVisualStyleBackColor = true;
+            this.btnPrueba.Click += new System.EventHandler(this.btnPrueba_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPrueba);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrueba;
+    }
+}
+
